f7e7bb050a8a7d82a8ad9089c425de12af937f89
Jesse Boucher <jesseboucher8@gmail.com>
2017-06-28 19:37:30 -0600

    completed project

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+
+// create array for messags
+	// var messages = [
+	// 	"What's happening!",
+	// 	"You probably want to start chatting, huh.",
+	// 	"Oh shiz, here we go!"
+	// ];
+
+	var messages = [];
+	// try adding username
+	var user = [];
+
+// Create object of our array of messages
+// messages.push({
+// 	message: req.body.message,
+// 	time: new Date()
+// });
+
+
+// GET endpoint that returns array of messages
+app.get('/messages', function(req, res, next){
+	res.status(200).json({ messages: messages});
+});
+
+// POST a new message
+app.post('/messages', function(req, res, next) {
+	messages.push({message: req.body.message, time: new Date() });
+	user.push({username: req.body.username, name: req.body.name})
+	res.status(200).json({messages: messages});
+})
+
+
+// create listening port
+var port = 3000;
+app.listen(port, function() {
+	console.log('listening on port', port)
+});
